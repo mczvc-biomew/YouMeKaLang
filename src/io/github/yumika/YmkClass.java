3ff583f9425cd67b12b2747e8e6e8df4107235b8
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-14 18:24:08 +0800

    [0.0.3] [YmkCallable] YmkClass

---
+++ src/io/github/yumika/YmkClass.java
@@ -0,0 +1,52 @@
+package io.github.yumika;
+
+import java.util.List;
+import java.util.Map;
+
+class YmkClass implements YmkCallable {
+  final String name;
+  final YmkClass superclass;
+
+  private final Map<String, YmkFunction> methods;
+
+  YmkClass(String name, YmkClass superclass,
+           Map<String, YmkFunction> methods) {
+    this.superclass = superclass;
+    this.name = name;
+    this.methods = methods;
+  }
+
+  YmkFunction findMethod(String name) {
+    if (methods.containsKey(name)) {
+      return methods.get(name);
+    }
+
+    if (superclass != null) {
+      return superclass.findMethod(name);
+    }
+
+    return null;
+  }
+
+  @Override
+  public String toString() { return name; }
+
+  @Override
+  public Object call(Interpreter interpreter,
+                     List<Object> arguments) {
+    YmkInstance instance = new YmkInstance(this);
+    YmkFunction initializer = findMethod("init");
+    if (initializer != null) {
+      initializer.bind(instance).call(interpreter, arguments);
+    }
+
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+    YmkFunction initializer = findMethod("init");
+    if (initializer == null) return 0;
+    return initializer.arity();
+  }
+}
