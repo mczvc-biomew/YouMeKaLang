73e3e54ceed6e8cc12fcedc6183826f11a843129
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-15 14:44:28 +0800

    # [0.0.8] * add UndefinedException * add ReferenceError

--- src/io/github/yumika/RuntimeError.java
+++ src/io/github/yumika/RuntimeError.java
@@ -3,6 +3,18 @@ package io.github.yumika;
 public class RuntimeError extends RuntimeException {
   final Token token;
 
+  static class UndefinedException extends RuntimeError {
+    UndefinedException(Token name, String message) {
+      super(name, message);
+    }
+  }
+
+  static class ReferenceError extends RuntimeError {
+    ReferenceError(Token name, String message) {
+      super(name, message);
+    }
+  }
+
   RuntimeError(Token token, String message) {
     super(message);
     this.token = token;
