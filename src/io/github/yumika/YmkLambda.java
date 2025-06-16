a828e5068f9873f34376b8abb51319570591bed7
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-16 21:45:09 +0800

    # [0.0.8] * add thisContext

--- src/io/github/yumika/YmkLambda.java
+++ src/io/github/yumika/YmkLambda.java
@@ -3,26 +3,35 @@ package io.github.yumika;
 import java.util.List;
 
 class YmkLambda implements YmkCallable {
-  private final Expr.Lambda lambda;
+  private final Expr.Lambda declaration;
   private final Environment closure;
+  private final Object thisContext;
 
-  public YmkLambda(Expr.Lambda lambda, Environment closure) {
-    this.lambda = lambda;
+  public YmkLambda(Expr.Lambda declaration, Environment closure, Object thisContext) {
+    this.declaration = declaration;
     this.closure = closure;
+    this.thisContext = thisContext;
   }
 
   @Override
   public int arity() {
-    return lambda.params.size();
+    return declaration.params.size();
+  }
+
+  public YmkLambda bind(Object newThis) {
+    return new YmkLambda(this.declaration, this.closure, newThis);
   }
 
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
     Environment environment = new Environment(closure);
-    for (int i = 0; i < lambda.params.size(); i++) {
-      environment.define(lambda.params.get(i).lexeme, arguments.get(i));
+    for (int i = 0; i < declaration.params.size(); i++) {
+      environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
-    return interpreter.evaluateExpr(lambda.body, environment);
+    if (thisContext != null) {
+      environment.define("this", thisContext);
+    }
+    return interpreter.evaluateExpr(declaration.body, environment);
   }
 
   @Override
