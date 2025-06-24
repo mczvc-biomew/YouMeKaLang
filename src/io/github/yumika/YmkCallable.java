4bacc8d382817cb299661335e7d5305ff93c026d
Meldencio Czarlemagne <mczvc@proton.me>
2025-06-24 11:54:36 +0800

    * change to public interface

--- src/io/github/yumika/YmkCallable.java
+++ src/io/github/yumika/YmkCallable.java
@@ -2,7 +2,7 @@ package io.github.yumika;
 
 import java.util.List;
 
-interface YmkCallable {
+public interface YmkCallable {
   int arity();
-  Object call(Interpreter interpreter, List<Object> arguments);
+  Object call(Interpreter interpreter, List<Object> args);
 }
